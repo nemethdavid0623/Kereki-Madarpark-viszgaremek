935f77a664a23feb6f08deacf44b4b64eddf175a
KOVRoland <roland.kovacs1219@gmail.com>
2026-01-26 12:47:43 +0100

    fejléc és navbar

--- Frontend/frontend/src/components/Layout.jsx
+++ Frontend/frontend/src/components/Layout.jsx
@@ -4,7 +4,23 @@ function Layout() {
   return (
     <body>
         <header className="header">
-            <h1>Balatoni madárkert</h1>
+            <img src="/madarpark_header.png" alt="" />
+           <div className='header-banner'>
+        <nav className="navbar">
+          <div className="nav-icon-wrapper">
+            
+          </div>
+          
+          <ul className="nav-links">
+            <a href="http://localhost:5173/"><img src="/logo.png" alt="" className='logo'/></a>
+            <li><a href="#">Tartási <br /> tanácsok</a></li>
+            <li><a href="#">Állataink</a></li>
+            <li><a href="#">Eladó <br /> pédányaink</a></li>
+            <li><a href="#">Rólunk</a></li>
+            <li><a href="#">Admin</a></li>
+          </ul>
+        </nav>
+      </div>
         </header>
     
         <footer className="footer">xyxx</footer>
--- Frontend/frontend/src/components/MainPage.jsx
+++ Frontend/frontend/src/components/MainPage.jsx
@@ -3,7 +3,30 @@ import './MainPage.css';
 
 const MainPage = () => {
   return (
+    
     <div className="main-wrapper">
+      
+      
+      <img src="/madarpark_header.png" alt="" />
+
+      <div className='header-banner'>
+        <nav className="navbar">
+          <div className="nav-icon-wrapper">
+            
+          </div>
+          
+          <ul className="nav-links">
+            <a href="http://localhost:5173/"><img src="/logo.png" alt="" className='logo'/></a>
+            <li><a href="#">Tartási <br /> tanácsok</a></li>
+            <li><a href="#">Állataink</a></li>
+            <li><a href="#">Eladó <br /> pédányaink</a></li>
+            <li><a href="#">Rólunk</a></li>
+            <li><a href="#">Admin</a></li>
+          </ul>
+        </nav>
+      </div>
+      
+
       {/* Üdvözlő sáv */}
       <div className="header-banner">
         <h2>Üdvözlünk a Balatoni Madárkertben!</h2>
