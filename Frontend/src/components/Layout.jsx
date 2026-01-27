62c3dbf3a61303d7378664f2ad2a4f35fc9212ec
Németh Dávid <endeka3636@gmail.com>
2026-01-27 18:38:57 +0100

    Állataink és Fóoldal folytatása

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -5,30 +5,22 @@ import './MainPage.css';
 function Layout() {
   return (
     <div className="main-wrapper">
-        <header className="header">
-            <img src="/madarpark_header.png" alt="" />
-           <div className='header-banner'>
-        <nav className="navbar">
-          <div className="nav-icon-wrapper">
-          </div>
-          
+        <img src="/madarpark_header.png" alt="Balatoni Madárkert" className='header-img'/>
+        
+        <nav className="header-banner-nav">
           <ul className="nav-links">
-            <a href="http://localhost:5173/"><img src="/logo.png" alt="" className='logo'/></a>
-            <li><a href="#">Tartási <br /> tanácsok</a></li>
+            <Link to="http://localhost:5173/"><img src="/logo.png" alt="" className='logo'/></Link>
+            <li><a href="#">Tartási<br/>tanácsok</a></li>
             <li><Link to="/allataink">Állataink</Link></li>
-            <li><a href="#">Eladó <br /> példányaink</a></li>
+            <li><a href="#">Eladó <br/>példányaink</a></li>
             <li><a href="#">Rólunk</a></li>
             <li><a href="#">Admin</a></li>
           </ul>
         </nav>
-      </div>
-        </header>
-
-      <main className="content-outlet">
+     
         <Outlet />
-      </main>
     
-        <footer className="footer">xyxx</footer>
+        <footer className="footer">footer</footer>
     </div>
   );
 }
--- Frontend/src/components/MainPage.jsx
+++ Frontend/src/components/MainPage.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
 import './MainPage.css';
 
-
 const MainPage = () => {
   return (
-    
-    <div className="main-wrapper">
-
+    <div className="main-page-content">
       <div className="header-banner">
         <h2>Üdvözlünk a Balatoni Madárkertben!</h2>
       </div>
       
-      <div className="description-banner">-*9
+      <div className="description-banner">
         <p>rövid leírás</p>
       </div>
 
-      <div className="content-container">
-        
-        <div className="content-row">
-          <div className="image-placeholder">kép</div>
-          <div className="text-placeholder">szöveg</div>
-        </div>
-
-        <div className="content-row reverse">
-          <div className="image-placeholder">kép</div>
-          <div className="text-placeholder">szöveg</div>
-        </div>
-
-        <div className="content-row">
-          <div className="image-placeholder">kép</div>
-          <div className="text-placeholder">szöveg</div>
-        </div>
-
+      <div className="content-row">
+        <div className="image-placeholder">kép</div>
+        <div className="text-placeholder">szöveg</div>
       </div>
 
+      <div className="content-row reverse">
+        <div className="image-placeholder">kép</div>
+        <div className="text-placeholder">szöveg</div>
+      </div>
+
+      <div className="content-row">
+        <div className="image-placeholder">kép</div>
+        <div className="text-placeholder">szöveg</div>
+      </div>
     </div>
   );
 };
